8118188325e539fa201db13e53275a55405a098e
Leland <the_square@tari.com>
2020-05-31 22:21:29 -0700

    Updates

---
+++ precache-manifest.e6b216fdc9636bbdbe6da9d5fe3164a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14878c59148981c33025ed902473bceb",
+    "url": "/governable/index.html"
+  },
+  {
+    "revision": "0f1950eb985e64988721",
+    "url": "/governable/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d83644946c7e05541ead",
+    "url": "/governable/static/js/2.f3e32eaf.chunk.js"
+  },
+  {
+    "revision": "4c81a6baa5d0e2a89c71c8ee0bdfec62",
+    "url": "/governable/static/js/2.f3e32eaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f1950eb985e64988721",
+    "url": "/governable/static/js/main.337c8aed.chunk.js"
+  },
+  {
+    "revision": "c1eeeb85a02b85e9feb1",
+    "url": "/governable/static/js/runtime-main.7f089328.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/governable/static/media/logo.5d5d9eef.svg"
+  }
+]);
