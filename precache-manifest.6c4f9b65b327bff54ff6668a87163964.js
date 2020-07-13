968939d7957321aaa5e51713ba7b1f3f2d415722
Leland <the_square@tari.com>
2020-07-13 11:22:26 -0700

    Updates

---
+++ precache-manifest.6c4f9b65b327bff54ff6668a87163964.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28d78cede2ae052670f8c55870d48d85",
+    "url": "/governable/index.html"
+  },
+  {
+    "revision": "e09fcc12376ba4ad5a06",
+    "url": "/governable/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "46c1afd5a761794f8784",
+    "url": "/governable/static/js/2.0593b1eb.chunk.js"
+  },
+  {
+    "revision": "4c81a6baa5d0e2a89c71c8ee0bdfec62",
+    "url": "/governable/static/js/2.0593b1eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e09fcc12376ba4ad5a06",
+    "url": "/governable/static/js/main.00a71351.chunk.js"
+  },
+  {
+    "revision": "c1eeeb85a02b85e9feb1",
+    "url": "/governable/static/js/runtime-main.7f089328.js"
+  },
+  {
+    "revision": "2616be68e6e7c7198a1564b535253582",
+    "url": "/governable/static/media/ABOUT.2616be68.md"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/governable/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ef2385c0a238c3f00b2917cac5ea9023.js"
+  "/governable/precache-manifest.6c4f9b65b327bff54ff6668a87163964.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/governable/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
