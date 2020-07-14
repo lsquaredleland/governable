541fa66924e2d06a6e97ca0f99296ee7ea0bdca4
Leland <the_square@tari.com>
2020-07-14 07:46:27 -0700

    Updates

--- precache-manifest.ec33ef75ead2be39e1d470995d0dfb44.js
+++ precache-manifest.ec33ef75ead2be39e1d470995d0dfb44.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2069a60896302f24e827280a012a1d58",
+    "revision": "215aa4dc31697eee07b830e6b3cf937a",
     "url": "/index.html"
   },
   {
-    "revision": "972a7d2e84a8eac0494c",
+    "revision": "6f1aaef960efd99ba108",
     "url": "/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "9fd23a4d7da1ba7fc71d",
-    "url": "/static/js/2.a9d46ed7.chunk.js"
+    "revision": "40009f92fce4b3422688",
+    "url": "/static/js/2.598d77c9.chunk.js"
   },
   {
     "revision": "4c81a6baa5d0e2a89c71c8ee0bdfec62",
-    "url": "/static/js/2.a9d46ed7.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.598d77c9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "972a7d2e84a8eac0494c",
-    "url": "/static/js/main.aa75449d.chunk.js"
+    "revision": "6f1aaef960efd99ba108",
+    "url": "/static/js/main.b670ac1d.chunk.js"
   },
   {
     "revision": "a990978c0f388b99584e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fa6ec643bb6f898d70cb70040ac01bfd.js"
+  "/precache-manifest.ec33ef75ead2be39e1d470995d0dfb44.js"
 );
 
 self.addEventListener('message', (event) => {
